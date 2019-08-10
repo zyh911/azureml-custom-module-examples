eb2fe7183745f9b10b1be67cb83ee50392b204ab
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-08-10 13:53:01 +0800

    Upload SS Module

--- semantic-segmentation-deeplabv3/script/score.py
+++ semantic-segmentation-deeplabv3/script/score.py
@@ -14,53 +14,25 @@ from azureml.studio.common.datatable.data_table import DataTable
 import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
-from .Model import ESPNet_Encoder, ESPNet
 
 
-class SSESPNet:
+class SSDeeplabv3:
     def __init__(self, model_path='saved_model', meta={}):
-        self.classes = 20
-        self.pallete = [
-            [128, 64, 128],
-            [244, 35, 232],
-            [70, 70, 70],
-            [102, 102, 156],
-            [190, 153, 153],
-            [153, 153, 153],
-            [250, 170, 30],
-            [220, 220, 0],
-            [107, 142, 35],
-            [152, 251, 152],
-            [70, 130, 180],
-            [220, 20, 60],
-            [255, 0, 0],
-            [0, 0, 142],
-            [0, 0, 70],
-            [0, 60, 100],
-            [0, 80, 100],
-            [0, 0, 230],
-            [119, 11, 32],
-            [0, 0, 0]
-        ]
-        self.mean = [72.3923111, 82.90893555, 73.15840149]
-        self.stdv = [45.3192215, 46.15289307, 44.91483307]
+        palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+        colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+        self.colors = (colors % 255).numpy().astype("uint8")
+
+        self.mean = [0.485, 0.456, 0.406]
+        self.stdv = [0.229, 0.224, 0.225]
         self.inference_transforms = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=self.mean, std=self.stdv),
         ])
-        self.up = None
-        self.p = int(meta['P'])
-        self.q = int(meta['Q'])
-        if meta['Model Type'] == 'ESPNet':
-            self.model = ESPNet(classes=self.classes, p=self.p, q=self.q)
-        else:
-            self.model = ESPNet_Encoder(self.classes, p=self.p, q=self.q)
-            self.up = torch.nn.Upsample(scale_factor=8, mode='bilinear')
+        self.model = torch.hub.load('pytorch/vision', 'deeplabv3_resnet101', pretrained=False)
+
         self.model.load_state_dict(torch.load(os.path.join(model_path, 'model.pth'), map_location='cpu'))
         if torch.cuda.is_available():
             self.model = self.model.cuda()
-            if self.up:
-                self.up = self.up.cuda()
             if torch.cuda.device_count() > 1:
                 self.model = torch.nn.DataParallel(self.model).cuda()
 
@@ -81,14 +53,11 @@ class SSESPNet:
                 input_tensor = input_tensor.cuda()
 
             with torch.no_grad():
-                output = self.model(input_tensor)
-                if self.up:
-                    output = self.up(output)
-                output = torch.argmax(output, 1)[0].cpu().numpy()
-                resultImg = np.zeros((output.shape[0], output.shape[1], 3), dtype=np.uint8)
-                for idx in range(len(self.pallete)):
-                    resultImg[output == idx] = self.pallete[idx]
-                resultImg1 = Image.fromarray(resultImg)
+                output = self.model(input_tensor)['out'][0]
+                output_predictions = output.argmax(0)
+
+                resultImg1 = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(img.size)
+                resultImg1.putpalette(self.colors)
                 resultImg2 = Image.blend(img, resultImg1, 0.5)
                 imgByteArr1 = BytesIO()
                 imgByteArr2 = BytesIO()
@@ -120,8 +89,8 @@ class SSESPNet:
 
 
 def test(args):
-    meta = {'Model Type': args.model_type, 'P': str(args.p), 'Q': str(args.q)}
-    ssespnet = SSESPNet(args.model_path, meta)
+    meta = {}
+    ssespnet = SSDeeplabv3(args.model_path, meta)
     ssespnet.evaluate(data_path=args.data_path, save_path=args.save_path)
 
     # Dump data_type.json as a work around until SMT deploys
@@ -146,11 +115,8 @@ def test(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--model_type', default='ESPNet', help='Model name')
     parser.add_argument('--data_path', default='script/outputs', help='Data directory')
     parser.add_argument('--save_path', default='script/outputs2', help='directory to save the results')
     parser.add_argument('--model_path', default='script/saved_model', help='Model directory')
-    parser.add_argument('-p', default=2, type=int, help='depth multiplier')
-    parser.add_argument('-q', default=8, type=int, help='depth multiplier')
 
     test(parser.parse_args())
